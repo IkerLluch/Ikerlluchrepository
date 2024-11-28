9b2878a3320ed1db54999fcce716c61bbd3bb047
iker <illuch@cendrassos.net>
2024-11-28 18:17:20 +0100

    missatge

---
+++ Ovelles negres2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Ovelles_negres2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] ovelles =
+        {
+            "blanca",
+            "negra",
+            "blanca",
+            "blanca",
+            "negra",
+            "blanca",
+            "negra",
+            "negra",
+        };
+        
+        if (ovelles.Length % 2 != 0)
+        {
+            Console.WriteLine("Aquesta llista no està bé");
+            return;
+        }
+        int totalOvellesNegras = 0;
+        int totalOvellesBlanques = 0;
+        int totalOvellesBlancasNegras = 0;
+        
+        for (int i = 0; i < ovelles.Length / 2; i++)
+        {
+            var primeravolta = ovelles[i];
+            var segonavolta = ovelles[(ovelles.Length - 1) - i];
+            if (primeravolta == segonavolta)
+            {
+                if (primeravolta == "negra")
+                {
+                    totalOvellesNegras++;
+                }
+                else if (primeravolta == "blanca")
+                {
+                    totalOvellesBlanques++;
+                }
+            }
+            else if (primeravolta == "blanca" && segonavolta == "negra")
+            {
+                totalOvellesBlancasNegras++;
+            }
+        }
+        Console.WriteLine($"Total ovelles negras: {totalOvellesNegras}");
+        Console.WriteLine($"Total ovelles blanques: {totalOvellesBlanques}");
+        Console.WriteLine($"Total ovelles blancas i negras: {totalOvellesBlancasNegras}");
+    }
+}
+//balbalo 99
